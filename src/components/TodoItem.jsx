2497c7fee72a6aea7b8cb24cb82b7657854e42fe
meddeb23 <dalimeddeb132@gmial.com>
2021-10-26 23:13:56 +0100

    final version

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -5,7 +5,7 @@ export default function TodoItem({ item, action }) {
     <li
       onDoubleClick={() => action(item.id)}
       className="py-4 px-6 my-2 rounded-md bg-white"
-      style={{ textDecoration: item.completed ? "line-through" : "" }}
+      style={{ back: item.completed ? "line-through" : "" }}
     >
       {item.title}
     </li>
