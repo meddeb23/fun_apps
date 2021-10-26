65ee6f7c98454d439e99ad25511c0a8e406796b9
meddeb23 <dalimeddeb132@gmial.com>
2021-10-26 21:56:12 +0100

    adding todos

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,9 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
+
+const TODOS = [
+  {
+    userId: 1,
+    id: 1,
+    title: "delectus aut autem",
+    completed: false,
+  },
+  {
+    userId: 1,
+    id: 2,
+    title: "quis ut nam facilis et officia qui",
+    completed: false,
+  },
+  {
+    userId: 1,
+    id: 3,
+    title: "fugiat veniam minus",
+    completed: false,
+  },
+];
 
 export default function Body() {
+  const [todoList, settodoList] = useState(TODOS);
+
   return (
-    <div>
-      <h1>Hello world 2</h1>
+    <div className="bg-gray-900 w-screen h-screen flex justify-start items-center">
+      <ul className="w-9/12 m-auto">
+        {todoList.map((todo) => (
+          <li key={todo.id} className="py-4 px-6 m-2 rounded-md bg-white">
+            {todo.title}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
